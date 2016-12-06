45dc0de70755c6475fe47b32dffa15e850a0c08a
Joao Cesar <joaocesarfernandes@gmail.com>
2016-12-06 08:34:42 -0200

    Funcionando

--- Template/src/app/componentes/conteudo/conteudo.component.ts
+++ Template/src/app/componentes/conteudo/conteudo.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import {Http, Response} from '@angular/http';
 
 import 'rxjs/add/operator/map';
+import { Usuario } from '../../usuario';
 
 @Component({
     selector: 'conteudo',
     templateUrl: './conteudo.component.html'
 })
-export class ConteudoComponent implements OnInit {
+export class ConteudoComponent {
     springUrl = 'http://localhost:8080/';
     conteudo;
+    sub;
 
-    constructor(private _http:Http) { }
+    constructor(private _http:Http) { 
+        this.sub = this._http.get(this.springUrl)
+                             .map(res => <Usuario>res.json())
+                             .subscribe(usu => this.conteudo = usu,
+                                        error => console.log(error));
+    }
 
-    ngOnInit() {
-        this.conteudo = this._http.get(this.springUrl).map(res => res.json().data as string[]);
-     }
 }
